7068311167bd31fdcba8eca78ae544169cee54d2
marc <marxdr7@gmail.com>
2023-09-07 12:00:28 -0300

    finished project

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 // init project
 var express = require('express');
 var app = express();
+require('dotenv').config();
+
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -22,6 +24,24 @@ app.get("/", function (req, res) {
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
+});
+
+app.get("/api/:date?", function (req, res) {
+  const dateParam = req.params.date;
+  let date;
+  if (!dateParam) {
+    date = new Date();
+  } else {
+    date = isNaN(dateParam) ? new Date(dateParam) : new Date(parseInt(dateParam));
+  }
+
+
+  if (isNaN(date.getTime())) return res.json({ error: "Invalid Date" });
+  
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
 });
 
 
